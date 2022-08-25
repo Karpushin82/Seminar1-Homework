a80fc87dd35aef8188318011cb1188b249dc6c39
Kirill <kiril-karp4@mail.ru>
2022-08-25 15:32:10 +1000

    Задача 4 решена

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишите программу,которая на вход принимает два числа и выдаёт какое большее,а какое меньшее
-
+/*
 Console.WriteLine("Введите первое число ");
 
 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -17,3 +17,47 @@ else if (num1 < num2)
 {
     Console.WriteLine("Второе число больше первого ");
 }
+*/
+
+//Задача №4 Написать программу которая на вход принимает три числа
+//и выдаёт максимальное из этих чисел
+
+/*
+Console.WriteLine("Введите первое число ");
+
+ int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число ");
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("Первое число больше второго ");
+
+}
+
+else if (num1 < num2)
+{
+    Console.WriteLine("Второе число больше первого ");
+}
+
+if(num2 > num3)
+{
+    Console.WriteLine("Второе число больше Третьего ");
+
+}
+
+else if (num2 < num3)
+{
+    Console.WriteLine("Третье число больше второго ");
+
+}
+
+*/
+
+
